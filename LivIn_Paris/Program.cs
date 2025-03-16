a62c9ce36d340d3020ce51664af3f3fe57bb6f2f
BaptistePalasi <Baptiste.palasi@edu.devinci.fr>
2025-03-16 16:27:31 +0100

    Update Program.cs

--- LivIn_Paris/Program.cs
+++ LivIn_Paris/Program.cs
@@ -12,6 +12,7 @@ namespace LivIn_Paris
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            test
         }
     }
 }
